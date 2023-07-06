03699c11595c8415f4bff32cfb09792d0c8fafa7
ueda <waley168@gmail.com>
2023-07-06 16:34:31 +0800

    fix: store miss

--- store/index.js
+++ store/index.js
@@ -1,11 +1,15 @@
 export const state = () => ({
   fromLocation: {},
+  toLocation: {},
   // 其他的 state
 });
 
 export const mutations = {
   setFromLocation(state, location) {
     state.fromLocation = location;
+  },
+  setToLocation(state, location) {
+    state.toLocation = location;
   },
   // 其他的 mutations
 };
